ecbf2569be4f6c92dde251a1ef08c86984c9d6e4
Sam Morris <sam.morris81@gmail.com>
2015-11-01 15:50:22 -0600

    adding in checkStrict

--- usefulfunctions.R
+++ usefulfunctions.R
@@ -55,3 +55,24 @@ transform <- list(
     function(x, ...) do.call(this.dens, args = list(pnorm(x), ...))
   }
 )
+
+# Checks a function for use of global variables
+# Returns TRUE if ok, FALSE if globals were found.
+checkStrict <- function(f, silent=FALSE) {
+  vars <- codetools::findGlobals(f)
+  found <- !vapply(vars, exists, logical(1), envir=as.environment(2))
+  names <- names(found)[found]
+  
+  if ((length(names) > 0)) {
+    sum.nfncs <- 0
+    for (i in 1:length(names)) {
+      if(!is.function(eval(parse(text=names[i])))) {sum.nfncs <- sum.nfncs + 1}
+    }
+    if (sum.nfncs > 0) {
+      warning("global variables used: ", paste(names(found)[found], collapse=', '))
+      return(invisible(FALSE))
+    }
+  }
+  
+  !any(found)
+}
